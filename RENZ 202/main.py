a91f548c128b3906287f3ea16c16d28f78beaebb
Nehe <133280575+Nbrown2309@users.noreply.github.com>
2024-05-27 00:10:40 +1200

    Updated File RENZ (Reupdated)

---
+++ RENZ 202/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/findagent')
+def findagent():
+    return render_template('findagent.html')
+
+@app.route('/homeinspiration')
+def homeinspiration():
+    return render_template('homeinspiration.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/myhome')
+def myhome():
+    return render_template('myhome.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
